c18d4302169e7e2f89b1d8a146342bb7fed3a236
Shashank Kumar Srivastava <srivastavashashank033@gmail.com>
2024-02-11 19:47:36 +0530

    UI bindings

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firestore_tutorial/services/database_service.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -8,10 +9,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  final DatabaseService _databaseService = DatabaseService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: _appBar(),
+      body: _buildUI(),
+    );
+  }
 
+  PreferredSizeWidget _appBar(){
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      title: const Center(
+          child: Text(
+            "Todos List",
+            style:TextStyle(
+              color:Colors.white70,
+            )
+          ),),
+    );
+  }
+
+  Widget _buildUI(){
+    return SafeArea(
+        child: Column(
+          children: [
+            _messagesListView(),
+          ],
+        ),
+    );
+  }
+
+  Widget _messagesListView(){
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height * 0.80,
+      width: MediaQuery.sizeOf(context).height,
+      child: StreamBuilder(
+        builder: (context,snapshot){
+          List todos = snapshot.data?.docs ?? [];
+          if(todos.isEmpty){
+            return const Center(
+              child: Text(
+                  "No todos for the day!! Add one",
+                style:TextStyle(
+                  color:Colors.black45,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            );
+          }
+          print(todos);
+          return ListView();
+        },
+        stream:_databaseService.getTodos()
+      ),
     );
   }
 }
--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,19 +1,27 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../models/todo.dart';
 
 const String TODO_COLLECTION_REF = "todos";
 
-class DatabaseService{
-
+class DatabaseService {
   final _firestore = FirebaseFirestore.instance;
   late final CollectionReference _todosRef;
 
-  DatabaseService(){
-   _todosRef = _firestore.collection(TODO_COLLECTION_REF).
-                withConverter<Todo>(
-                  fromFirestore: fromFirestore,
-                  toFirestore: toFirestore);
+  DatabaseService() {
+    _todosRef = _firestore.collection(TODO_COLLECTION_REF).withConverter<Todo>(
+          fromFirestore: (snapshots, _) => Todo.fromJson(
+            snapshots.data()!,
+          ),
+          toFirestore: (todo, _) => todo.toJson(),
+        );
+  }
+
+  Stream<QuerySnapshot> getTodos(){
+    return _todosRef.snapshots();
+  }
+
+  void addTodo(Todo todo) async{
+   _todosRef.add(todo);
   }
 }
